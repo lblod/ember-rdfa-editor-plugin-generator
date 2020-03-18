a7c080cccb08f62071bb8a9b021ba9775b762ca8
Aad Versteden <madnificent@gmail.com>
2020-03-18 21:16:25 +0100

    Simplify service
    The service provides a certain structure.  We imposed the structure to
    get started and saw it as a value.  It turns out not all plugins
    benefit from such structure and it may be easier to reason about a
    plugin without it.  This is largely due to a much cleaned API space.
    
    There is still room for improvement and we intend to tackle a bunch of
    it before releasing this revamped generator.

--- blueprints/editor-plugin/files/addon/services/rdfa-editor-__name__-plugin.js
+++ blueprints/editor-plugin/files/addon/services/rdfa-editor-__name__-plugin.js
@@ -32,44 +32,29 @@ export default class RdfaEditor<%= classifiedModuleName %>Plugin extends Service
     if (rdfaBlocks.length === 0) return [];
 
     const hints = [];
-    /* --- Detect relevant rdfa block --- */
-    rdfaBlocks.forEach((rdfaBlock) => {
-      let relevantRdfaBlock = this.detectRelevantRdfaBlock(rdfaBlock);
-      if (relevantRdfaBlock) {
-        hintsRegistry.removeHintsInRegion(rdfaBlock.region, hrId, this.get('who'));
-        hints.pushObjects(this.generateHintsForRdfaBlock(rdfaBlock));
-      }
-    });
-    /* --- A helper also exists to find rich nodes in the rdfa blocks with specific rdfa attributes : --- */
-    /*const richNodes = editor.findUniqueRichNodes(
-      rdfaBlocks,
-      {
-        resource: '...',
-        property: '...',
-        typeof: '...' ,
-        datatype: '...'
-      }
-    );*/
 
-    const cards = hints.map( (hint) => this.generateCard(hrId, hintsRegistry, editor, hint));
-    if(cards.length > 0){
-      hintsRegistry.addHints(hrId, this.get('who'), cards);
+    for( const rdfaBlock of rdfaBlocks ){
+      hintsRegistry.removeHintsInRegion(rdfaBlock.region, hrId, this.who);
+
+      let idx = rdfaBlock.text.toLowerCase().indexOf('hello');
+      if( idx !== -1 ) {
+        // the hintsregistry needs to know the location with respect to the document
+        const absoluteLocation = this.normalizeLocation( [idx, idx + 'hello'.length], rdfaBlock.region );
+
+        hints.push( {
+          // info for the hintsRegistry
+          location: absoluteLocation,
+          card: this.who,
+          // any content you need to render the component and handle its actions
+          info: {
+            hrId, hintsRegistry, editor,
+            location: absoluteLocation
+          }
+        });
+      }
     }
-  }
-
-  /**
-   * Given a rdfa block, tries to detect a block the plugin can work on
-   *
-   * @method detectRelevantRdfaBlock
-   *
-   * @param {Object} rdfaBlock Text snippet at a specific location with an RDFa context
-   *
-   * @return {String} URI of rdfa block if found, else empty string.
-   *
-   * @private
-   */
-  detectRelevantRdfaBlock(rdfaBlock) {
-    return rdfaBlock.text.toLowerCase().indexOf('hello') >= 0;
+    <%= "TODO: document editor.findUniqueRichNodes and reference editor docs" && "" %>
+    hintsRegistry.addHints(hrId, this.who, hints);
   }
 
   /**
@@ -86,53 +71,5 @@ export default class RdfaEditor<%= classifiedModuleName %>Plugin extends Service
    */
   normalizeLocation(location, reference) {
     return [location[0] + reference[0], location[1] + reference[0]];
-  }
-
-  /**
-   * Generates a card given a hint
-   *
-   * @method generateCard
-   *
-   * @param {string} hrId Unique identifier of the event in the hintsRegistry
-   * @param {Object} hintsRegistry Registry of hints in the editor
-   * @param {Object} editor The RDFa editor instance
-   * @param {Object} hint containing the hinted string and the location of this string
-   *
-   * @return {Object} The card to hint for a given template
-   *
-   * @private
-   */
-  generateCard(hrId, hintsRegistry, editor, hint) {
-    return EmberObject.create({
-      info: {
-        label: this.get('who'),
-        plainValue: hint.text,
-        htmlString: '<b>hello world</b>',
-        location: hint.location,
-        hrId, hintsRegistry, editor
-      },
-      location: hint.location,
-      card: this.get('who')
-    });
-  }
-
-  /**
-   * Generates a hint, given a rdfa block
-   *
-   * @method generateHintsForRdfaBlock
-   *
-   * @param {Object} rdfaBlock Text snippet at a specific location with an RDFa context
-   *
-   * @return {Object} [{dateString, location}]
-   *
-   * @private
-   */
-  generateHintsForRdfaBlock(rdfaBlock) {
-    const hints = [];
-    const index = rdfaBlock.text.toLowerCase().indexOf('hello');
-    const text = rdfaBlock.text.slice(index, index+5);
-    const location = this.normalizeLocation([index, index + 5], rdfaBlock.region);
-    hints.push({text, location});
-    return hints;
   }
 }
