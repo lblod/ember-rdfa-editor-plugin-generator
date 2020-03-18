9c8274a831ad64caa38a33b7b2f72c4754df0bcf
Aad Versteden <madnificent@gmail.com>
2020-03-18 20:15:00 +0100

    Don't create variables for things under info
    We're making it obvious that the info section is where you get your
    information from.  Repeating may be more obvious than creating a
    variable for it.

--- blueprints/editor-plugin/files/addon/components/editor-plugins/__name__-card.js
+++ blueprints/editor-plugin/files/addon/components/editor-plugins/__name__-card.js
@@ -12,9 +12,8 @@ export default class <%= classifiedModuleName %>Card extends Component {
   @action
   insert() {
     const info = this.args.info;
-    const hintsRegistry = info.hintsRegistry;
-    hintsRegistry.removeHintsAtLocation( info.location, info.hrId, 'editor-plugins/<%= dasherizedModuleName %>-card' );
-    const mappedLocation = hintsRegistry.updateLocationToCurrentIndex(info.hrId, info.location);
+    info.hintsRegistry.removeHintsAtLocation( info.location, info.hrId, 'editor-plugins/<%= dasherizedModuleName %>-card' );
+    const mappedLocation = info.hintsRegistry.updateLocationToCurrentIndex(info.hrId, info.location);
     const selection = info.editor.selectHighlight( mappedLocation );
     info.editor.update( selection, {
       set: { innerHTML: 'my <a href="https://say-editor.com">Say Editor</a> hint card' }
